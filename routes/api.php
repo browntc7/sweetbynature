e4543046cc2111ea519380e4f8b18c8bd9b8c20e
Sam Slosser <Samuel.Slosser@tec-usa.com>
2019-01-28 11:17:59 -0500

    Removed passport for now added section for class to put in routes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::get('/user', function (Request $request) {
         return $request->user();
     });
 Route::get('/test','ApiController@getTest');
-
-
 });
 
+
+//Class put your API routes below this comment
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Class put your Web routes below this comment
