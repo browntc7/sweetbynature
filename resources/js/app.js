8ee5f8c63a963e2b9254a46347e9ec5d0c1f7ecb
Samuel Slosser <samuel.slosser@gmail.com>
2019-04-10 20:37:07 -0400

    moved views from layouts fixed vue.js bug added eslint

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,10 +17,10 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +28,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+// eslint-disable-next-line no-undef
 const app = new Vue({
-    el: '#app'
+  el: '#app'
 });
