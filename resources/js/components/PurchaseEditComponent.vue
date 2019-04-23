fb240059e5e3d5d2be0e6872733052a9349f0bd2
@c2designer! <cedmonds1@my.apsu.edu>
2019-04-23 17:00:55 -0500

    added purchase order edit component

---
+++ resources/js/components/PurchaseEditComponent.vue
@@ -0,0 +1,71 @@
+<template>
+    <form @submit.prevent="submit">
+  <div class="form-group row">
+    <label for="Date" class="col-sm-offset-2 col-sm-2 col-form-label">Date</label>
+    <div class="col\-sm\-6">
+      <input type="text" class="form-control" id="Date" placeholder="Date">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="purchaseOrder" class="col-sm-offset-2 col-sm-2 col-form-label">Purchase Order #</label>
+    <div class="col\-sm\-6">
+      <input type="text" class="form-control" id="purchaseOrder" placeholder="purchaseOrder">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="customerNumber" class="col-sm-offset-2 col-sm-2 col-form-label">Customer #</label>
+    <div class="col\-sm\-6">
+      <input type="text" class="form-control" id="customerNumber" placeholder="customerNumber">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="status" class="col-sm-offset-2 col-sm-2 col-form-label">Status</label>
+    <div class="col\-sm\-6">
+      <input type="text" class="form-control" id="status" placeholder="status">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="item" class="col-sm-offset-2 col-sm-2 col-form-label">Item</label>
+    <div class="col\-sm\-6">
+      <input type="text" class="form-control" id="item" placeholder="item">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="Qty" class="col-sm-offset-2 col-sm-2 col-form-label">Qty</label>
+    <div class="col\-sm\-6">
+      <input type="text" class="form-control" id="Qty" placeholder="Qty">
+    </div>
+  </div>
+        <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+       </div>
+        
+    </form>
+
+</template>
+<script>
+export default {
+  //data for the vue instance 
+  data() {
+    return {
+      //set copy_shipping to true so its checked other views need only fields:{}
+      fields: {copy_shipping:true},
+      errors: {},
+    }
+  },
+  methods: {
+    submit() {
+      this.errors = {};
+      axios.post('/api/addCustomer', this.fields).then(response => {
+        //hide the modal on the view
+      $("#purchaseOrderModal").modal("hide");
+      //reload table data and sort using the table name variable
+       customerTable.ajax.reload().order([0,"desc"]);
+      }).catch(error => {
+        alert("The Transaction Failed on the Server")
+      });
+    },
+  },
+}
+</script>
