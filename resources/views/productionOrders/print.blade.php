7534c3f23c68ebabb793614066a60f488af6d975
@c2designer! <cedmonds1@my.apsu.edu>
2019-04-25 10:28:10 -0500

    edit popover content

--- resources/views/productionOrders/print.blade.php
+++ resources/views/productionOrders/print.blade.php
@@ -50,14 +50,14 @@
      <button class="btn btn-primary" type="reset">Cancel</button>
      <button class="btn btn-primary"type="submit">Submit</button>
 </div>
-    <footer class="footer">
+    <!-- <footer class="footer">
       <div class="container">
         <span class="text-muted" style="float:right;">      
         <a href="#" data-toggle="help" title="Need Help?" data-content=""> 
         <i class="fa fa-question-circle" style="font-size:24px;color:red;" aria-hidden="true"></i></a>
        </span>
       </div>
-    </footer>
+    </footer> -->
 @endsection
 
 @section('footScripts')
